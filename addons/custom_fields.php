2f3bdffa7c2433e0071aa41c95137775607e7665
sumitramanga <sumitramanga@gmail.com>
2018-12-02 22:29:39 +1300

    setting up save metaboxes function but not working

--- addons/custom_fields.php
+++ addons/custom_fields.php
@@ -30,6 +30,8 @@ function show_metaboxes($post, $args) {
 	global $metaboxes;
 
 	$fields = $metaboxes[$args['id']]['fields'];
+	$custom_values = get_post_custom($post->ID);
+	$output = '<input type="hidden" name="post_format_meta_box_nonce" value="'.wp_create_nonce(basename(__FILE__)).'">';
 
 	if ( ! empty($fields) ) {
 		foreach ($fields as $id => $field) {
@@ -60,3 +62,47 @@ function show_metaboxes($post, $args) {
 	}
 	echo $output;
 }
+
+// Saving to the database
+function save_metaboxes($post_id){
+	global $metaboxes;
+
+	if ( ! wp_verify_nonce( $_POST['post_format_meta_box_nonce'], basename(__FILE__)) ) {
+		return $post_id;
+	}
+
+	if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) {
+		return $post_id;
+	}
+
+	if ($_POST['post_type'] == 'page') {
+		if ( ! current_user_can('edit_page', $post_id ) ) {
+			return $post_id;
+		}
+	} elseif ( ! current_user_can('edit_page', $post_id ) ) {
+		return $post_id;
+	}
+
+	$post_type = get_post_type();
+
+	foreach ( $metaboxes as $id => $metabox ) {
+		// die($post_type);
+		if ( $metabox['applicableto'] == $post_type ) {
+			$fields = $metaboxes[$id]['fields'];
+
+			foreach ($fields as $id => $field) {
+				$old_value = get_post_meta($post_id, $id, true);
+				$new_value = $_POST[$id];
+
+				if ($new_value && $new_value != $old_value) {
+					update_post_meta($post_id, $id, $new_value);
+				} elseif ($new_value == '' && $old_value || !isset($_POST[$id])) {
+					delete_post_meta($post_id, $id, $old_value);
+				}
+			}
+
+		}
+	}
+}
+
+add_action('save_post', 'save_metaboxes');
--- services-template.php
+++ services-template.php
@@ -1,6 +1,5 @@
 <?php /* Template name: Services Template */ ?>
 
 <?php get_header(); ?>
-<h1>this is the services template</h1>
-
+	<h1>this is the services template</h1>
 <?php get_footer(); ?>
