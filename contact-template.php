1062d59dd6611e1dffa3cc20558452face120de1
sumitramanga <sumitramanga@gmail.com>
2018-12-05 11:31:28 +1300

    Created form validation for contact page form and outputted messages for errors and successful form sent

--- contact-template.php
+++ contact-template.php
@@ -1,8 +1,64 @@
 <?php /* Template name: Enquiries/contact Template */ ?>
 
-<?php get_header(); ?>
+<?php
+	if ($_POST) {
+		// var_dump($_POST);
 
-	<h1>contact template</h1>
+		$errors = array();
+		$success_messsage = '';
+
+		if (!wp_verify_nonce($_POST['_wpnonce'], 'wp_enquiry_form')) {
+			echo("We cannot save the data at this time. Please try again later.");
+			return;
+		}
+
+		if (strlen($_POST['userName']) <= 2) {
+			array_push($errors, 'Name is too short. Please enter your full name');
+		} elseif (strlen($_POST['userName']) >= 90) {
+			array_push($errors, 'Name is too long.');
+		}
+
+		if (strlen($_POST['userEmail']) <= 1) {
+			array_push($errors, 'Email is too short. Please enter a valid email.');
+		}
+
+		if ($_POST['reasonForContact'] == 'Choose an option') {
+			array_push($errors, 'Please enter your reasoning for contacting us');
+		}
+
+		if (strlen($_POST['enquirySubject']) <= 5) {
+			array_push($errors, 'Please enter a more descriptive subject');
+		} elseif (strlen($_POST['enquierySubject']) >= 100) {
+			array_push($errors, 'Your subject is too long. Please enter a shorter subject.');
+		}
+
+		if (strlen($_POST['userMessage']) <= 5) {
+			array_push($errors, 'Please enter a more descriptive message');
+		} elseif (strlen($_POST['userMessage']) >= 400) {
+			array_push($errors, 'Please enter a shorter message');
+		}
+
+		if (empty($errors)) {
+			$args = array(
+				'post_title' => $_POST['enquirySubject'],
+				'post_content' => $_POST['userMessage'],
+				'post_type' =>	'enquiries',
+				'meta_input' =>	array(
+					'full_name' => $_POST['userName'],
+					'email' => $_POST['userEmail'],
+					'message_reason' => $_POST['reasonForContact']
+				)
+			);
+
+			wp_insert_post($args);
+			$success_message = 'Thank you for your message. We will get back to you shortly';
+		}
+
+	}
+
+ ?>
+
+<?php get_header(); ?>
 
 	<?php if(have_posts()): ?>
 		<?php while(have_posts()): the_post(); ?>
@@ -11,17 +67,33 @@
 					<p><?php the_content(); ?></p>
 				</div>
 
+				<?php if ($_POST && !empty($errors)): ?>
+					<div class="row">
+						<div class="col">
+							<ul>
+								<?php foreach($errors as $singleError): ?>
+									<li><?php echo($singleError); ?></li>
+								<?php endforeach; ?>
+							</ul>
+						</div>
+					</div>
+				<?php endif; ?>
+
+				<?php if($_POST && empty($errors)): ?>
+					<?php echo $success_message; ?>
+				<?php endif; ?>
+
 				<div class="row">
 					<div class="col">
 						<form class="contanctForm" action="<?php get_permalink(); ?>" method="post">
 							<?php wp_nonce_field('wp_enquiry_form'); ?>
 							<div class="form-group">
 								<label for="userName">Full Name</label>
-								<input type="text" class="form-control" id="userName" name="userName" placeholder="Name"/>
+								<input type="text" class="form-control" id="userName" name="userName"/>
 							</div>
 							<div class="form-group">
 								<label for="userEmail">Email</label>
-								<input type="email" class="form-control" id="userEmail" name="userEmail" placeholder="Email"/>
+								<input type="email" class="form-control" id="userEmail" name="userEmail"/>
 							</div>
 
 							<div class="form-group">
@@ -35,8 +107,8 @@
 							</div>
 
 							<div class="form-group">
-								<label for="enquierySubject">Subject</label>
-								<input type="text" class="form-control" id="enquierySubject" name="enquierySubject"/>
+								<label for="enquirySubject">Subject</label>
+								<input type="text" class="form-control" id="enquirySubject" name="enquirySubject"/>
 							</div>
 
 							<div class="form-group">
